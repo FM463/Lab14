8afc30d1a6e47d8a66699e0108fe4956008f72fc
FM463 <fatimamustafaeva055@gmail.com>
2021-11-10 23:03:35 +0200

    Lab14

---
+++ Lab14/7.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab14
+{
+    public class Taxi
+    {
+        public static bool IsInitialized;
+
+        public Taxi()
+        {
+            IsInitialized = true;
+        }
+    }
+
+    class TestTaxi
+    {
+        static void Main()
+        {
+            Taxi t = new Taxi();
+            Console.WriteLine(t.IsInitialized);
+        }
+    }
+}
---
+++ Lab14/9.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab14
+{
+    class _9
+    {
+        public class Taxi
+        {
+            private static bool IsInitialized;
+            public static int i = 5;
+
+            private Taxi()
+            {
+                IsInitialized = true;
+            }
+        }
+
+        class TestTaxi
+        {
+            static void Main()
+            {
+                Taxi t = new Taxi();
+                Console.WriteLine(t.IsInitialized);
+                i = Console.ReadLine();
+            }
+        }
+    }
+}
